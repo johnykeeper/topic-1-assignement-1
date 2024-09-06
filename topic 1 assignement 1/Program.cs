c50a68cf00acf95070bf9e35629863e83d4c242a
Jonathan Gaboury <362568651@GoTVDSB.ca>
2024-09-06 08:56:46 -0400

    completed assigment

--- topic 1 assignement 1/Program.cs
+++ topic 1 assignement 1/Program.cs
@@ -5,8 +5,20 @@
         static void Main(string[] args)
         {
             //jonathan
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("finished 100%");
+            Console.WriteLine("Hello, World! my names Jonathan");
+            Console.WriteLine();
+            Console.WriteLine("comments are used to explain something in your code to someone who is looking at it");
+            Console.WriteLine();
+            Console.WriteLine("jjjjj   ggg");
+            Console.WriteLine("  j    g   g");
+            Console.WriteLine("  j    g     ");
+            Console.WriteLine("  j    ggggg");
+            Console.WriteLine("j j    g   g");
+            Console.WriteLine("j j    g   g");
+            Console.WriteLine(" jj     ggg");
+            Console.WriteLine();
+            Console.WriteLine(" I just made my initials");
+            
         }
     }
 }
